5a790385e010e04797163285d1d8d0d6e4c6e98a
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-03-10 16:05:07 +0000

    spire:1.11.2

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -17,35 +17,8 @@ dl()
 dlver () {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver linux-amd64-glibc
-    dl $ver linux-arm64-glibc
+    dl $ver linux-amd64-musl
+    dl $ver linux-arm64-musl
 }
 
-dlver ${1:-1.7.6}
-#dlver 1.8.0
-#dlver 1.8.1
-#dlver 1.8.2
-#dlver 1.8.3
-#dlver 1.8.4
-#dlver 1.8.5
-#dlver 1.8.6
-#dlver 1.8.7
-#dlver 1.8.8
-#dlver 1.8.9
-#dlver 1.8.10
-#dlver 1.8.11
-#dlver 1.9.0
-#dlver 1.9.1
-#dlver 1.9.2
-#dlver 1.9.3
-#dlver 1.9.4
-#dlver 1.9.5
-#dlver 1.9.6
-#dlver 1.10.0
-#dlver 1.10.1
-#dlver 1.10.2
-#dlver 1.10.3
-#dlver 1.10.4
-#dlver 1.11.0
-#dlver 1.11.1
-#dlver ${1:-1.11.2}
+dlver ${1:-1.11.2}
